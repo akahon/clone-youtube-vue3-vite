f06a65d4174a75b69623f549678e07225aec6e63
Fayzullo Saidakbarov <akahon1222@gmail.com>
2022-04-26 14:34:25 +0500

    Created primary components

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheSideBarSmall from './components/TheSideBarSmall.vue';
+import TheSidebar from './components/TheSidebar.vue';
+import TheCategories from './components/TheCategories.vue';
+import TheVideos from './components/TheVideos.vue';
+import TheSidebarMobile from './components/TheSidebarMobile.vue';
+
+</script>
+
+<template>
+  <TheHeader />
+  <TheSideBarSmall />
+  <TheSidebar />
+  <TheSidebarMobile />
+  <TheCategories />
+  <TheVideos />
+</template>
